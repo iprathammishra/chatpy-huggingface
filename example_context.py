93a47ddd6585f12cb38e2a7279521d31a68a29d2
Pratham <mishrapratham.codeit@gmail.com>
2023-06-15 17:58:27 +0530

    Example context

---
+++ example_context.py
@@ -0,0 +1,20 @@
+exContext = """
+gravity, also called gravitation, in mechanics, the universal force of attraction acting between all matter. It is by far the weakest known force in nature and thus plays no role in determining the internal properties of everyday matter. 
+On the other hand, through its long reach and universal action, it controls the trajectories of bodies in the solar system and elsewhere in the universe and the structures and evolution of stars, galaxies, and the whole cosmos. 
+On Earth all bodies have a weight, or downward force of gravity, proportional to their mass, which Earth’s mass exerts on them. Gravity is measured by the acceleration that it gives to freely falling objects. 
+At Earth’s surface the acceleration of gravity is about 9.8 metres (32 feet) per second per second. Thus, for every second an object is in free fall, its speed increases by about 9.8 metres per second. At the surface of the Moon the acceleration of a freely falling body is about 1.6 metres per second per second.
+Understand the concept of gravitational force using Newton's theory of gravitation
+The works of Isaac Newton and Albert Einstein dominate the development of gravitational theory. 
+Newton’s classical theory of gravitational force held sway from his Principia, published in 1687, until Einstein’s work in the early 20th century. Newton’s theory is sufficient even today for all but the most precise applications. 
+Einstein’s theory of general relativity predicts only minute quantitative differences from the Newtonian theory except in a few special cases. The major significance of Einstein’s theory is its radical conceptual departure from classical theory and its implications for further growth in physical thought.
+The launch of space vehicles and developments of research from them have led to great improvements in measurements of gravity around Earth, other planets, and the Moon and in experiments on the nature of gravitation.
+Development of gravitational theory
+Early concepts
+Newton argued that the movements of celestial bodies and the free fall of objects on Earth are determined by the same force. 
+The classical Greek philosophers, on the other hand, did not consider the celestial bodies to be affected by gravity, because the bodies were observed to follow perpetually repeating nondescending trajectories in the sky. 
+Thus, Aristotle considered that each heavenly body followed a particular “natural” motion, unaffected by external causes or agents. 
+Aristotle also believed that massive earthly objects possess a natural tendency to move toward Earth’s centre. 
+Those Aristotelian concepts prevailed for centuries along with two others: that a body moving at constant speed requires a continuous force acting on it and that force must be applied by contact rather than interaction at a distance. 
+These ideas were generally held until the 16th and early 17th centuries, thereby impeding an understanding of the true principles of motion and precluding the development of ideas about universal gravitation. 
+This impasse began to change with several scientific contributions to the problem of earthly and celestial motion, which in turn set the stage for Newton’s later gravitational theory.
+"""
